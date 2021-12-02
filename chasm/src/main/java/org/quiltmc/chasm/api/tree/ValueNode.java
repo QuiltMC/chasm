c3d28df56be450f06ed744f76f2e5b0f362bcca5
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-12-02 15:42:20 +0200

    Degenerify ValueNode (QuiltMC#33)
    Also implements "getValueAs" for checked casting of value

--- chasm/src/main/java/org/quiltmc/chasm/api/tree/ValueNode.java
+++ chasm/src/main/java/org/quiltmc/chasm/api/tree/ValueNode.java
@@ -2,21 +2,51 @@ package org.quiltmc.chasm.api.tree;
 
 import org.quiltmc.chasm.internal.metadata.MetadataProvider;
 
-public class ValueNode<T> implements Node {
-    private final T value;
+public class ValueNode implements Node {
+    private final Object value;
     private MetadataProvider metadataProvider = new MetadataProvider();
 
-    public ValueNode(T value) {
+    public ValueNode(Object value) {
         this.value = value;
     }
 
-    public T getValue() {
+    public Object getValue() {
         return value;
     }
 
+    public <T> T getValueAs(Class<T> type) {
+        if (value == null) {
+            return null;
+        }
+        if (!type.isInstance(value)) {
+            throw new IllegalStateException("Value is not of expected type " + type + ", but is " + value.getClass() + "!");
+        }
+        return type.cast(value);
+    }
+
+    public String getValueAsString() {
+        return getValueAs(String.class);
+    }
+
+    public int getValueAsInt() {
+        Integer boxed = getValueAs(Integer.class);
+        if (boxed == null) {
+            throw new IllegalStateException("Value is null, but primitives can't be null!");
+        }
+        return boxed;
+    }
+
+    public boolean getValueAsBoolean() {
+        Boolean boxed = getValueAs(Boolean.class);
+        if (boxed == null) {
+            throw new IllegalStateException("Value is null, but primitives can't be null!");
+        }
+        return boxed;
+    }
+
     @Override
-    public ValueNode<T> copy() {
-        ValueNode<T> copy = new ValueNode<>(value);
+    public ValueNode copy() {
+        ValueNode copy = new ValueNode(value);
         copy.metadataProvider = metadataProvider.copy();
         return copy;
     }
