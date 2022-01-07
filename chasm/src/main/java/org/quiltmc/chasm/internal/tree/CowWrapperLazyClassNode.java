ec9ac1d070f316e0a1899aafd452a3230383433c
cplir-c <31234188+cplir-c@users.noreply.github.com>
2022-01-07 15:28:06 -0600

    implement CowWrapperLazyClassNode#nonLazyEntrySet

--- chasm/src/main/java/org/quiltmc/chasm/internal/tree/CowWrapperLazyClassNode.java
+++ chasm/src/main/java/org/quiltmc/chasm/internal/tree/CowWrapperLazyClassNode.java
@@ -4,20 +4,17 @@
 package org.quiltmc.chasm.internal.tree;
 
 import java.lang.ref.Reference;
-import java.lang.ref.ReferenceQueue;
 import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+import java.util.WeakHashMap;
 
 import org.objectweb.asm.ClassReader;
-import org.quiltmc.chasm.api.tree.CowWrapperNode;
 import org.quiltmc.chasm.api.tree.MapNode;
 import org.quiltmc.chasm.api.tree.Node;
-import org.quiltmc.chasm.internal.collection.ReadOnlyIteratorWrapper;
-import org.quiltmc.chasm.internal.collection.WeakValueHashMap;
 
 /**
  *
@@ -34,14 +31,14 @@ public class CowWrapperLazyClassNode extends CowWrapperMapNode implements LazyCl
      * @param <W>
      * @param parent
      * @param key
-     * @param lazyClassMapNode
+     * @param lazyClassNode
      * @param owned
      */
-    public <K, P extends Node, W extends AbstractCowWrapperNode<P, W>> CowWrapperLazyClassNode(
+    public <P extends Node, W extends AbstractCowWrapperNode<P, W>> CowWrapperLazyClassNode(
             AbstractCowWrapperNode<P, W> parent,
-            K key, LazyClassMapNode lazyClassMapNode,
+            Object key, LazyClassNode lazyClassNode,
             boolean owned) {
-        super(parent, key, lazyClassMapNode, owned);
+        super(parent, key, lazyClassNode, owned);
     }
 
     /**
@@ -53,14 +50,25 @@ public class CowWrapperLazyClassNode extends CowWrapperMapNode implements LazyCl
 
     @Override
     public CowWrapperLazyClassNode deepCopy() {
-        // TODO Auto-generated method stub
-        return null;
+        CowWrapperLazyClassNode copy = new CowWrapperLazyClassNode(this);
+        copy.toShared();
+        copy.toOwned();
+        if (!this.isOwned()) {
+            copy.toShared();
+        }
+        copy.object = this.object.deepCopy();
+        if (this.fullNodeCache != null) {
+            CowWrapperMapNode wrapped = this.fullNodeCache.get();
+            if (wrapped != null) {
+                copy.fullNodeCache = new SoftReference<>(wrapped.deepCopy());
+            }
+        }
+        return copy;
     }
 
     @Override
     public CowWrapperLazyClassNode shallowCopy() {
-        // TODO Auto-generated method stub
-        return null;
+        return new CowWrapperLazyClassNode(this);
     }
 
     @Override
@@ -92,13 +100,13 @@ public class CowWrapperLazyClassNode extends CowWrapperMapNode implements LazyCl
     private static final class CowWrapperLazyClassNodeNonLazyEntry implements Entry<String, Node> {
         private final CowWrapperLazyClassNodeNonLazyEntrySet parentSet;
         private final String key;
-        private SoftReference<Entry<String, Node>> wrapped;
+        private Reference<Entry<String, Node>> wrapped;
 
-        CowWrapperLazyClassNodeNonLazyEntry(CowWrapperLazyClassNodeNonLazyEntrySet parent, Entry<String, Node> wrapped,
-                ReferenceQueue<Entry<String, Node>> queue) {
+        CowWrapperLazyClassNodeNonLazyEntry(CowWrapperLazyClassNodeNonLazyEntrySet parent,
+                Entry<String, Node> wrapped) {
             this.parentSet = parent;
             this.key = wrapped.getKey();
-            this.wrapped = new SoftReference<>(wrapped, queue);
+            this.wrapped = new WeakReference<>(wrapped);
         }
 
         @Override
@@ -115,8 +123,7 @@ public class CowWrapperLazyClassNode extends CowWrapperMapNode implements LazyCl
                 return null;
             }
 
-            Node value = entry.getValue();
-            return value;
+            return entry.getValue();
         }
 
         @Override
@@ -128,101 +135,102 @@ public class CowWrapperLazyClassNode extends CowWrapperMapNode implements LazyCl
 
     private static final class CowWrapperLazyClassNodeNonLazyEntrySet implements Set<Entry<String, Node>> {
         private final CowWrapperLazyClassNode self;
-        private final Set<Entry<String, Node>> wrappedSet;
-        private final Map<String, Entry<String, Node>> map;
-        private final Map<String, CowWrapperNode> wrapperCache = null;
 
-        private Map<String, CowWrapperLazyClassNodeNonLazyEntry> entryCache = null;
-        private ReferenceQueue<Entry<String, Node>> deadEntryQueue = null;
+        private Map<Entry<String, Node>, CowWrapperLazyClassNodeNonLazyEntry> entryCache = null;
 
-        CowWrapperLazyClassNodeNonLazyEntrySet(CowWrapperLazyClassNode self, Set<Entry<String, Node>> set) {
+        CowWrapperLazyClassNodeNonLazyEntrySet(CowWrapperLazyClassNode self) {
             this.self = self;
-            this.wrappedSet = set;
-            this.map = CowWrapperLazyClassNodeNonLazyEntrySet.initializeMap(wrappedSet);
         }
 
-        /**
-         * @param wrappedSet2
-         *
-         * @return
-         */
-        private static Map<String, Entry<String, Node>> initializeMap(Set<Entry<String, Node>> wrappedSet) {
-            Map<String, Entry<String, Node>> map = new WeakValueHashMap<String, Node>(wrappedSet.size()) {
-
-            };
-            return map;
+        private Set<Entry<String, Node>> getWrappedSet() {
+            return this.self.getLazyClassNodeObject().getNonLazyEntrySet();
         }
 
         @Override
         public int size() {
-            return this.wrappedSet.size();
+            return this.getWrappedSet().size();
         }
 
         @Override
-        public boolean isEmpty() { return this.wrappedSet.isEmpty(); }
+        public boolean isEmpty() { return this.getWrappedSet().isEmpty(); }
 
         @Override
         public boolean contains(Object o) {
-            return this.wrappedSet.contains(o);
+            return this.getWrappedSet().contains(o);
+        }
+
+        private static final class CowWrapperLazyClassNodeNonLazyEntrySetIterator
+                implements Iterator<Entry<String, Node>> {
+            CowWrapperLazyClassNodeNonLazyEntrySet parent;
+            Set<Entry<String, Node>> sourceSet;
+            Iterator<Entry<String, Node>> wrapped;
+            int skipCount = 0;
+
+            CowWrapperLazyClassNodeNonLazyEntrySetIterator(
+                    CowWrapperLazyClassNodeNonLazyEntrySet entrySet) {
+                this.parent = entrySet;
+                this.sourceSet = this.parent.getWrappedSet();
+                this.wrapped = sourceSet.iterator();
+            }
+
+            private void ensureMatchingSource() {
+                if (this.sourceSet != this.parent.getWrappedSet()) {
+                    this.sourceSet = this.parent.getWrappedSet();
+                    this.wrapped = sourceSet.iterator();
+                    for (int i = 0; i < skipCount; ++i) {
+                        wrapped.next();
+                    }
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                this.ensureMatchingSource();
+                return this.wrapped.hasNext();
+            }
+
+            @Override
+            public Entry<String, Node> next() {
+                this.ensureMatchingSource();
+                Entry<String, Node> entryKey = this.wrapped.next();
+                ++skipCount;
+                return this.parent.getEntryWrapper(entryKey);
+            }
         }
 
         @Override
         public Iterator<Entry<String, Node>> iterator() {
-            return new ReadOnlyIteratorWrapper<>(this.wrappedSet.iterator());
+            return new CowWrapperLazyClassNodeNonLazyEntrySetIterator(this);
         }
 
-        @Override
-        private CowWrapperLazyClassNodeNonLazyEntry getCachedEntry(String key) {
+        private CowWrapperLazyClassNodeNonLazyEntry getCachedEntryWrapper(Entry<String, Node> entry) {
             if (this.entryCache == null) {
                 return null;
             }
-            return this.entryCache.get(key);
+            return this.entryCache.get(entry);
         }
 
-        private CowWrapperLazyClassNodeNonLazyEntry getEntry(Entry<String, Node> wrap) {
-            String key = wrap.getKey();
-            CowWrapperLazyClassNodeNonLazyEntry entry = this.getCachedEntry(key);
+        private CowWrapperLazyClassNodeNonLazyEntry getEntryWrapper(Entry<String, Node> wrap) {
+            CowWrapperLazyClassNodeNonLazyEntry entry = this.getCachedEntryWrapper(wrap);
             if (entry == null) {
-                entry = new CowWrapperLazyClassNodeNonLazyEntry(this, wrap, this.deadEntryQueue);
-                this.setEntry(entry);
+                entry = new CowWrapperLazyClassNodeNonLazyEntry(this, wrap);
+                this.setEntry(wrap, entry);
             }
             return entry;
         }
 
-        private void setEntry(CowWrapperLazyClassNodeNonLazyEntry entry) {
+        private void setEntry(Entry<String, Node> keyEntry, CowWrapperLazyClassNodeNonLazyEntry entry) {
             if (this.entryCache == null) {
-                this.entryCache = new HashMap<>();
+                this.entryCache = new WeakHashMap<>();
             }
-            this.entryCache.put(entry.getKey(), entry);
-        }
-
-        private void removeCachedEntry(String key, Node value) {
-            if (this.entryCache != null) {
-                CowWrapperLazyClassNodeNonLazyEntry entry = this.entryCache.remove(key);
-                Node containedValue = entry.getValue();
-                if (containedValue != value && containedValue != null && !containedValue.equals(value)) {
-                    this.entryCache.put(key, entry);
-                }
-            }
-            if (this.wrapperCache != null) {
-                this.wrapperCache.remove(key, value);
-            }
+            this.entryCache.put(keyEntry, entry);
         }
 
         private void requireFullyGeneratedEntryCache() {
-            if (this.entryCache.size() < this.wrappedSet.size()) {
-                for (Entry<String, Node> entry : this.wrappedSet) {
-                    this.getEntry(entry);
-                }
-            }
-            if (this.entryCache.size() > this.wrappedSet.size()) {
-                Reference<? extends Entry<String, Node>> deadEntry = deadEntryQueue.poll();
-                while (deadEntry != null) {
-                    if (deadEntry.refersTo(null)) {
-                        throw new AssertionError();
-                    }
-                    this.removeCachedEntry(deadEntry.get().getKey(), deadEntry.get().getValue());
-                    deadEntry = deadEntryQueue.poll();
+            // just checking the size of the entry cache clears stale entries
+            if (this.entryCache.size() < this.getWrappedSet().size()) {
+                for (Entry<String, Node> entry : this.getWrappedSet()) {
+                    this.getEntryWrapper(entry);
                 }
             }
         }
@@ -242,61 +250,94 @@ public class CowWrapperLazyClassNode extends CowWrapperMapNode implements LazyCl
         @Override
         public boolean add(Entry<String, Node> e) {
             Node value = e.getValue();
-            if (!this.self.toOwned()) {
-                this.removeCachedEntry(e.getKey(), value);
+            return getOwnedEntrySet().add(e);
+        }
+
+        private Set<Entry<String, Node>> getOwnedEntrySet() {
+            Set<Entry<String, Node>> set = this.getWrappedSet();
+            if (this.self.toOwned()) {
+                Set<Entry<String, Node>> newSet = this.getWrappedSet();
+                if (newSet != set) {
+                    this.entryCache = null;
+                    set = newSet;
+                }
             }
-            return this.wrappedSet.add(e);
+            return set;
         }
 
         @Override
         public boolean remove(Object o) {
-            // TODO Auto-generated method stub
+            if (this.getOwnedEntrySet().remove(o)) {
+                if (this.entryCache != null) {
+                    this.entryCache.remove(o);
+                }
+                return true;
+            }
             return false;
         }
 
         @Override
         public boolean containsAll(Collection<?> c) {
-            // TODO Auto-generated method stub
-            return false;
+            return this.getWrappedSet().containsAll(c);
         }
 
         @Override
         public boolean addAll(Collection<? extends Entry<String, Node>> c) {
-            // TODO Auto-generated method stub
-            return false;
+            return this.getOwnedEntrySet().addAll(c);
         }
 
         @Override
         public boolean retainAll(Collection<?> c) {
-            // TODO Auto-generated method stub
-            return false;
+            Iterator<Entry<String, Node>> iter = this.getOwnedEntrySet().iterator();
+            boolean changed = false;
+            for (Entry<String, Node> entry = iter.next(); iter.hasNext(); entry = iter.next()) {
+                if (c.contains(entry)) {
+                    iter.remove();
+                    if (this.entryCache != null) {
+                        this.entryCache.remove(entry);
+                    }
+                    changed = true;
+                }
+            }
+            return changed;
         }
 
         @Override
         public boolean removeAll(Collection<?> c) {
-            // TODO Auto-generated method stub
+            if (this.getOwnedEntrySet().removeAll(c)) {
+                if (this.entryCache != null) {
+                    this.entryCache.keySet().removeAll(c);
+                }
+                return true;
+            }
             return false;
         }
 
         @Override
         public void clear() {
-            // TODO Auto-generated method stub
-
+            this.getOwnedEntrySet().clear();
+            if (this.entryCache != null) {
+                this.entryCache.clear();
+            }
         }
 
     }
 
-
     @Override
     public Set<Entry<String, Node>> getNonLazyEntrySet() {
-
+        return new CowWrapperLazyClassNodeNonLazyEntrySet(this);
     }
 
     @Override
     public <P extends Node, W extends AbstractCowWrapperNode<P, W>> CowWrapperLazyClassNode asWrapper(
             AbstractCowWrapperNode<P, W> parent, Object key, boolean owned) {
-        // TODO Auto-generated method stub
-        return null;
+        if (this.wrapsObject(parent) && this.checkKey(key) && this.isOwned() == owned) {
+            return this;
+        }
+        CowWrapperLazyClassNode copy = new CowWrapperLazyClassNode(parent, key, this.getLazyClassNodeObject(),
+                this.isOwned());
+        copy.toOwned(owned);
+        return copy;
     }
 
 }
