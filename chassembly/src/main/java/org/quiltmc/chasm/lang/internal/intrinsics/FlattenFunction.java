00951f5c42da6fba370ad89a6a3a27ab15c65c63
William Bradford Larcombe <william@williambl.com>
2022-08-02 14:06:53 +0100

    Add intrinsics tests, implement `map` and `flatten` intrinsics (#57)
    * Add intrinsics tests
    
    * Add list node collector
    
    * Add map intrinsic
    
    * Add flatten intrinsic
    
    * Add test for chars/join intrinsics
    
    * Fix intrinsics checkstyle
    
    * Add some javadoc
    
    * Remove ListNode#collector
    
    * Rename `function` parameter to `func` in `map` intrinsic
    
    * Remove leftover import

---
+++ chassembly/src/main/java/org/quiltmc/chasm/lang/internal/intrinsics/FlattenFunction.java
@@ -0,0 +1,43 @@
+package org.quiltmc.chasm.lang.internal.intrinsics;
+
+import java.util.stream.Collectors;
+
+import org.quiltmc.chasm.lang.api.ast.ListNode;
+import org.quiltmc.chasm.lang.api.ast.Node;
+import org.quiltmc.chasm.lang.api.eval.Evaluator;
+import org.quiltmc.chasm.lang.api.exception.EvaluationException;
+
+/**
+ * The flatten intrinsic, which takes in a list of lists and returns a new list with the contents of the sublists.
+ */
+public class FlattenFunction extends IntrinsicFunction {
+    @Override
+    public Node apply(Evaluator evaluator, Node arg) {
+        if (!(arg instanceof ListNode)) {
+            throw createArgsException(arg);
+        }
+
+        return new ListNode(((ListNode) arg).getEntries()
+                .stream()
+                .map(entry -> {
+                    if (entry instanceof ListNode) {
+                        return (ListNode) entry;
+                    } else {
+                        throw createArgsException(arg);
+                    }
+                })
+                .flatMap(entry -> entry.getEntries().stream())
+                .collect(Collectors.toList()));
+    }
+
+    @Override
+    String getName() {
+        return "flatten";
+    }
+
+    private static EvaluationException createArgsException(Node arg) {
+        return new EvaluationException(
+                "Built-in function \"flatten\" can only be applied to lists of lists but found " + arg
+        );
+    }
+}
---
+++ chassembly/src/main/java/org/quiltmc/chasm/lang/internal/intrinsics/MapFunction.java
@@ -0,0 +1,46 @@
+package org.quiltmc.chasm.lang.internal.intrinsics;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.quiltmc.chasm.lang.api.ast.ListNode;
+import org.quiltmc.chasm.lang.api.ast.MapNode;
+import org.quiltmc.chasm.lang.api.ast.Node;
+import org.quiltmc.chasm.lang.api.eval.Evaluator;
+import org.quiltmc.chasm.lang.api.eval.FunctionNode;
+import org.quiltmc.chasm.lang.api.exception.EvaluationException;
+
+/**
+ * The map intrinsic, which takes in a list and a function, and returns a new list of the result of applying the
+ * function to each element in the list.
+ */
+public class MapFunction extends IntrinsicFunction {
+    @Override
+    public Node apply(Evaluator evaluator, Node arg) {
+        if (!(arg instanceof MapNode)) {
+            throw createArgsException(arg);
+        }
+
+        Map<String, Node> args = ((MapNode) arg).getEntries();
+        Node list = args.get("list");
+        Node function = args.get("func");
+        if (!(list instanceof ListNode) || !(function instanceof FunctionNode)) {
+            throw createArgsException(arg);
+        }
+
+        return new ListNode(((ListNode) list).getEntries().stream()
+                .map(entry -> ((FunctionNode) function).apply(evaluator, entry))
+                .collect(Collectors.toList()));
+    }
+
+    @Override
+    String getName() {
+        return "map";
+    }
+
+    private static EvaluationException createArgsException(Node arg) {
+        return new EvaluationException(
+                "Built-in function \"map\" can only be applied to args {list, func} but found " + arg
+        );
+    }
+}
