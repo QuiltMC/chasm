3505cdb400e7f75bc7262ef7687ba0622076164b
anonymous123-code <61744596+anonymous123-code@users.noreply.github.com>
2023-05-10 14:18:53 +0200

    Make SourceSpan nullable in EvaluationException

--- chassembly/src/main/java/org/quiltmc/chasm/lang/api/exception/EvaluationException.java
+++ chassembly/src/main/java/org/quiltmc/chasm/lang/api/exception/EvaluationException.java
@@ -1,5 +1,6 @@
 package org.quiltmc.chasm.lang.api.exception;
 
+import org.jetbrains.annotations.Nullable;
 import org.quiltmc.chasm.lang.api.eval.SourceSpan;
 
 /**
@@ -9,7 +10,16 @@ public class EvaluationException extends RuntimeException {
     /**
      * Creates an {@linkplain EvaluationException} with the given message.
      */
-    public EvaluationException(String message, SourceSpan span) {
-        super(message + "in " + span.asString());
+    public EvaluationException(String message) {
+        super(message);
+    }
+
+    /**
+     * Creates an {@linkplain EvaluationException} with the given message, thrown at the given source span.
+     * @param message the message
+     * @param span the source span, if null no source information will be included in the error message
+     */
+    public EvaluationException(String message, @Nullable SourceSpan span) {
+        super(message + (span != null ? (" in " + span.asString()) : ""));
     }
 }
